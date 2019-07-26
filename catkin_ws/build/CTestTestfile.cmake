ff29efa52f5fc8aad291707ca356f257a2f6233d
Your Name <you@example.com>
2019-07-26 15:30:02 +0200

    commit nr 1

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/user_en/catkin_ws/src
+# Build directory: /home/user_en/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("anro-team-9/beginner_tutorials")
---
+++ catkin_ws/build/anro-team-9/beginner_tutorials/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/user_en/catkin_ws/src/anro-team-9/beginner_tutorials
+# Build directory: /home/user_en/catkin_ws/build/anro-team-9/beginner_tutorials
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
